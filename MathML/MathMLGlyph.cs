c9f55def4696cae383cc38251c39205c9e957e49
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2016-03-20 21:32:36 +0000

    Long Division and Glyph

---
+++ MathML/MathMLGlyph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathML
+{
+    [MathMLElementName("mglyph")]
+    public class MathMLGlyph : MathMLElement
+    {
+        [MathMLAttributeName("alt")]
+        public string AlternativeText { get; set; }
+
+        [MathMLAttributeName("height")]
+        public MathMLLength Height { get; set; }
+
+        [MathMLAttributeName("src")]
+        public string Source { get; set; }
+
+        [MathMLAttributeName("valign")]
+        public MathMLLength VerticalAlignment { get; set; }
+
+        [MathMLAttributeName("width")]
+        public MathMLLength Width { get; set; }
+    }
+}
