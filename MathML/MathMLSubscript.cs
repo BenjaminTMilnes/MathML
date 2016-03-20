2b10b781ca5bd1d3cc567c11d6848b474fb76f5f
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2016-03-20 18:37:13 +0000

    Subscripts and Superscripts

---
+++ MathML/MathMLSubscript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathML
+{
+    [MathMLElementName("msub")]
+    public class MathMLSubscript : MathMLElement
+    {
+        [MathMLAttributeName("subscriptshift")]
+        public MathMLLength Shift { get; set; }
+    }
+}
---
+++ MathML/MathMLSuperscript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathML
+{
+    [MathMLElementName("msup")]
+    public class MathMLSuperscript : MathMLElement
+    {
+        [MathMLAttributeName("superscriptshift")]
+        public MathMLLength Shift { get; set; }
+    }
+}
